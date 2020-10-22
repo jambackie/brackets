b015ab58ac900e0e1d7d8286b9c1b6e83ecae3f6
jambackie <jambackie@gmail.com>
2020-10-22 16:16:17 +0300

    add task implement

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const arr = [],
+  open = [],
+  close = []
+  bracketsConfig.forEach((i) => {
+    open.push(i[0])
+    close.push(i[1])
+  })
+  for (let i = 0; i < str.length; i++) {
+    if (close.includes(str[i]) && arr.length !== 0) {
+      const bracket = open[close.indexOf(str[i])];
+      if (arr[arr.length - 1] === bracket || arr[arr.length - 1] === str[i]) {
+        arr.pop()
+      } else if (str[i] !== bracket) {
+        return false;
+      } else {
+        arr.push(str[i])
+      }
+    } else {
+      arr.push(str[i])
+    }
+  }
+  return arr.length === 0
 }
